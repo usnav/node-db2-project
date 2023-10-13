1e0acf007a62938a7c07c870f1f9987ed831fb23
usnar <usna.rafiqzadah@gmail.com>
2023-10-12 22:45:15 -0400

    mvp completed

--- api/server.js
+++ api/server.js
@@ -2,6 +2,23 @@ const express = require("express")
 
 const server = express()
 
-// DO YOUR MAGIC
+const carsRouter = require('./cars/cars-router')
+
+server.use(express.json())
+
+server.use('/api/cars', carsRouter)
+
+server.use('*', (req, res, next) => {
+    next({
+        status: 404, 
+        message: 'not found'
+    })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+res.status(err.status || 500).json({
+    message: err.message
+})
+})
 
 module.exports = server
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+
+const cars = [
+    {
+        vin: '1BAGNB7A3TF071506',
+        make: "toyota", 
+        model: "prius",
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    } ,
+    {
+        vin: 'TRUSC28N341016582',
+        make: "toyota", 
+        model: "corolla",
+        mileage: 115000,
+        title: 'salvage'
+    } ,
+    {
+        vin: 'WDBAB23ADCB324720',
+        make: "ford", 
+        model: "focus",
+        mileage: 15000,
+    } 
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars) 
+//     })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars) 
+}
